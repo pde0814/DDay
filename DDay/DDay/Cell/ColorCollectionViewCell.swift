18beb56573cef9808253ffc27a90a3565a01cc42
pde0814 <dongeon8943@gmail.com>
2025-05-27 16:06:03 +0900

    Step 9-1
    Compose View

--- DDay/DDay/Cell/ColorCollectionViewCell.swift
+++ DDay/DDay/Cell/ColorCollectionViewCell.swift
@@ -8,5 +8,8 @@
 import UIKit
 
 class ColorCollectionViewCell: UICollectionViewCell {
-    
+
+
+
+    @IBOutlet weak var colorImageView: UIImageView!
 }
--- DDay/DDay/ComposeViewController.swift
+++ DDay/DDay/ComposeViewController.swift
@@ -9,21 +9,56 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+    @IBOutlet weak var backgroundColorCollectionView: UICollectionView!
+    @IBOutlet weak var textColorCollectionView: UICollectionView!
+    @IBOutlet weak var titleField: UITextField!
+
+
+    @IBAction func save(_ sender: Any) {
+
+    }
+
+    let colors: [UIColor] = [
+        .systemRed,
+        .systemOrange,
+        .systemYellow,
+        .systemGreen,
+        .systemBlue,
+        .systemPurple,
+        .systemPink,
+        .systemTeal,
+        .systemIndigo,
+        .systemGray,
+        .black,
+        .white
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+}
+
+extension ComposeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors.count + 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: ColorCollectionViewCell.self), for: indexPath) as! ColorCollectionViewCell
+
+        if colors.count == indexPath.item {
+            cell.colorImageView.image = UIImage(named: "color-picker")
+            cell.colorImageView.tintColor = nil
+        } else {
+            // TODO: 템플릿 이미지 조사해보기
+            cell.colorImageView.image = UIImage(named: "color-picker")?.withRenderingMode(.alwaysTemplate)
+            cell.colorImageView.tintColor = colors[indexPath.item]
+        }
+
+        return cell
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
